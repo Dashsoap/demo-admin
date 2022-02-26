998a68f7b01f1824004a874b6bb33a31bf96ee0b
Dashsoap <360062589@qq.com>
2022-02-26 17:37:47 +0800

    feat: css add

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,24 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import eslintPlugin from 'vite-plugin-eslint'
+import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(),
     eslintPlugin({
       // 配置选项
       cache: false
-    })]
+    })],
+  resolve: {
+    alias: {
+      '@': path.join(__dirname, 'src')
+    }
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: '@inport "@/styles/variables.scss";'
+      }
+    }
+  }
 })
